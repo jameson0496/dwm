61a31fb2a5ab4de45a3c536da2aa3ba7a6ad154a
jameson0496 <jamie.d.wilson1964@gmail.com>
2025-08-15 22:34:59 -0400

    added viewontag patch

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int startontag         = 0;        /* 0 means no tag active on start */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
