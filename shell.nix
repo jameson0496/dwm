e1851c9e6b5fbd2e1c919da04911dbcc097891a1
jameson0496 <jamie.d.wilson1964@gmail.com>
2025-08-15 21:30:26 -0400

    downloaded all potential patches; added .gitignore; added nix shell for building.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.pkg-config
+    pkgs.fontconfig
+    pkgs.freetype
+    pkgs.xorg.libX11
+    pkgs.xorg.libXft
+    pkgs.xorg.libXinerama
+  ];
+}
